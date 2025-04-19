94c5b153b51ac37ac81d2a6d2418aa28903e1cb5
maksimmerkulov <merkulov.mv@outlook.com>
2025-04-19 12:59:09 +0500

    Погружаемся. Условное ветвление

--- README.md
+++ README.md
@@ -13,5 +13,6 @@
 - 📄 Упражнения из книги:
     - Стр. 39 — создание класса с методом main
     - Стр. 42 — пример цикла while
+    - Стр. 43 — условное ветвление
 
 ![Chapter 1](https://github.com/user-attachments/assets/7533dfdb-bb0a-4eb9-96e3-ba3e68f2a443)
---
+++ src/main/java/com/headfirstjava/chapter01/IfTest.java
@@ -0,0 +1,11 @@
+package com.headfirstjava.chapter01;
+
+public class IfTest {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x должен равняться 3");
+        }
+        System.out.println("Это строка выполняется в любом случае");
+    }
+}
