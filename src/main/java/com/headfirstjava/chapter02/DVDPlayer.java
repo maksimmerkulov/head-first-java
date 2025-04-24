4fca05640b04399937cfac46d4976461877db98d
maksimmerkulov <merkulov.mv@outlook.com>
2025-04-24 19:57:16 +0500

    Путешествие в Объектвилль. Поработайте компилятором

---
+++ src/main/java/com/headfirstjava/chapter02/DVDPlayer.java
@@ -0,0 +1,13 @@
+package com.headfirstjava.chapter02;
+
+public class DVDPlayer {
+    boolean canRecord = false;
+
+    void recordDVD() {
+        System.out.println("идет запись DVD");
+    }
+
+    void playDVD() {
+        System.out.println("DVD проигрывается");
+    }
+}
---
+++ src/main/java/com/headfirstjava/chapter02/TapeDeck.java
@@ -0,0 +1,13 @@
+package com.headfirstjava.chapter02;
+
+public class TapeDeck {
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println("пленка проигрывается");
+    }
+
+    void recordTape() {
+        System.out.println("идет запись на пленку");
+    }
+}
