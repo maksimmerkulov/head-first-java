316778ab2355fa205ccbbcdbe01bbd4207322212
maksimmerkulov <merkulov.mv@outlook.com>
2025-04-28 20:02:35 +0500

    Как себя ведут объекты. Разница между переменными экземпляра и локальными переменными

---
+++ src/main/java/com/headfirstjava/chapter04/Foo.java
@@ -0,0 +1,8 @@
+package com.headfirstjava.chapter04;
+
+public class Foo {
+    public void go() {
+        int x = 0;
+        int z = x + 3;
+    }
+}
---
+++ src/main/java/com/headfirstjava/chapter04/FooTestDrive.java
@@ -0,0 +1,8 @@
+package com.headfirstjava.chapter04;
+
+public class FooTestDrive {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        foo.go();
+    }
+}
