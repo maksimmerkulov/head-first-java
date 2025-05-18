820bd03f118c232dd94feb99d1aea844231228cc
maksimmerkulov <merkulov.mv@outlook.com>
2025-05-18 21:44:00 +0500

    Жизнь и смерть объектов. Упростите процесс создания объекта Duck

---
+++ src/main/java/com/headfirstjava/chapter09/UseADuck2.java
@@ -0,0 +1,8 @@
+package com.headfirstjava.chapter09;
+
+public class UseADuck2 {
+    public static void main(String[] args) {
+        Duck2 d = new Duck2(15);
+        Duck2 d2 = new Duck2();
+    }
+}
