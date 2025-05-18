08ceb9c61b502ad228d2d03cd4c3c4e7e168bc08
maksimmerkulov <merkulov.mv@outlook.com>
2025-05-18 20:48:38 +0500

    Жизнь и смерть объектов. Создаем объект Duck

---
+++ src/main/java/com/headfirstjava/chapter09/Duck.java
@@ -0,0 +1,7 @@
+package com.headfirstjava.chapter09;
+
+public class Duck {
+    public Duck() {
+        System.out.println("Кря");
+    }
+}
---
+++ src/main/java/com/headfirstjava/chapter09/UseADuck.java
@@ -0,0 +1,7 @@
+package com.headfirstjava.chapter09;
+
+public class UseADuck {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+    }
+}
