fd92ad18858de03694b492165c854e817bcf12cf
maksimmerkulov <merkulov.mv@outlook.com>
2025-05-26 20:47:45 +0500

    Числа имеют значение. Больше статического! Статический импорт

---
+++ src/main/java/com/headfirstjava/chapter10/WithStatic.java
@@ -0,0 +1,11 @@
+package com.headfirstjava.chapter10;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class WithStatic {
+    public static void main(String[] args) {
+        out.println("sqrt " + sqrt(2.0));
+        out.println("tan " + tan(60));
+    }
+}
